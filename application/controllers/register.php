30ffd3191ea0bcb1afaf91085632cad937d64331
Hipzul Achmad Jabbar <hipzulachmadjabbar@gmail.com>
2018-05-01 09:16:52 +0700

    Register Fix
    Udah Fix yang ini

--- application/controllers/register.php
+++ application/controllers/register.php
@@ -5,15 +5,53 @@ class Register extends CI_Controller {
 
 	function __construct(){
         parent::__construct();
+				$this->load->library('session');
+				$this->load->model('m_register');
+				$this->load->library('form_validation');
+				$this->load->helper('security');
     }
 
-	public function index()
-	{
+	public function index(){
 		$this->load->view('register');
 	}
 
 	function do_regist(){
-		echo "mau regis";
+		$this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+		$this->form_validation->set_rules('username', 'Username', 'trim|required');
+		$this->form_validation->set_rules('password', 'Password', 'trim|required');
+		$this->form_validation->set_rules('role', 'Role', 'trim|required');
+
+		if($this->form_validation->run() == FALSE){
+				$this->load->view('register');
+			}
+		else{
+				$data = array(
+						'email' => $this->input->post('email'),
+						'username' => $this->input->post('username'),
+						'password' => $this->input->post('password'),
+						'role' => $this->input->post('role'),
+				);
+				$cek = $this->m_register->cek_regist($data);
+					if($cek == false){
+						$data = array(
+							'error_regist' => 'Username or Email Already Registered');
+						$this->load->view('register', $data);
+						}
+					else{
+						$this->m_register->registration($data);
+						$data_session = array(
+							'nama' => $data['username'],
+							'status' => "login"
+						);
+						$this->session->set_userdata($data_session);
+						if ($data["role"] == "perusahaan") {
+							echo "halaman perusahaan";
+						}
+						elseif ($data["role"] == "organisasi") {
+							echo "halaman organisasi";
+						}
+			}
+		}
 	}
 }
 ?>
--- application/views/register.php
+++ application/views/register.php
@@ -5,26 +5,26 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->
-	<link rel="icon" type="image/png" href="assets/images/icons/favicon.ico"/>
+	<link rel="icon" type="image/png" href="<?php echo base_url();?>assets/images/icons/favicon.ico"/>
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="assets/vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="assets/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="assets/vendor/animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/vendor/animate/animate.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="assets/vendor/css-hamburgers/hamburgers.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/vendor/css-hamburgers/hamburgers.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="assets/vendor/animsition/css/animsition.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/vendor/animsition/css/animsition.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="assets/vendor/select2/select2.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/vendor/select2/select2.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="assets/vendor/daterangepicker/daterangepicker.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/vendor/daterangepicker/daterangepicker.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="assets/css/util.css">
-	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/util.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/main.css">
 <!--===============================================================================================-->
 </head>
 <body>
@@ -32,11 +32,20 @@
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100 p-l-85 p-r-85 p-t-55 p-b-55">
-				<form class="login100-form validate-form flex-sb flex-w" action="register/do_regist" method="POST">
+				<form class="login100-form validate-form flex-sb flex-w" action="<?php echo site_url('register/do_regist')?>" method="POST">
 
-					<span class="login100-form-title p-b-32">
+					<span class="login100-form-title p-b-12">
 						Register
 					</span>
+
+            <?php
+              echo "<div class='error_msg p-b-12'>";
+              if (isset($error_regist)) {
+                  echo $error_regist;
+                  }
+              echo validation_errors();
+              echo "</div>";
+            ?>
 
           <span class="txt1 p-b-11">
 						Email
@@ -65,8 +74,16 @@
 						<span class="focus-input100"></span>
 					</div>
 
+          <div class="validate-input m-b-12" data-validate = "Role is required">
+            <span class="txt1 p-b-11 ">
+              <p class="m-b-12">Saya Mendaftar Sebagai : </P>
+              <input type="radio" name="role" value="perusahaan">Perusahaan &nbsp;
+              <input type="radio" name="role" value="organisasi">Organisasi
+            </span>
+          </div>
+
 					<div class="container-login100-form-btn">
-						<input type="submit" value="Login" class="login100-form-btn">
+						<input type="submit" value="Register" class="login100-form-btn">
 						<!-- <button style="float: right;" type="button" class="login100-form-btn">Register</button> -->
 					</div>
 
@@ -83,21 +100,21 @@
 	<div id="dropDownSelect1"></div>
 
 <!--===============================================================================================-->
-	<script src="assets/vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src="<?php echo base_url();?>assets/vendor/jquery/jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
-	<script src="assets/vendor/animsition/js/animsition.min.js"></script>
+	<script src="<?php echo base_url();?>assets/vendor/animsition/js/animsition.min.js"></script>
 <!--===============================================================================================-->
-	<script src="assets/vendor/bootstrap/js/popper.js"></script>
-	<script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="<?php echo base_url();?>assets/vendor/bootstrap/js/popper.js"></script>
+	<script src="<?php echo base_url();?>assets/vendor/bootstrap/js/bootstrap.min.js"></script>
 <!--===============================================================================================-->
-	<script src="assets/vendor/select2/select2.min.js"></script>
+	<script src="<?php echo base_url();?>assets/vendor/select2/select2.min.js"></script>
 <!--===============================================================================================-->
-	<script src="assets/vendor/daterangepicker/moment.min.js"></script>
-	<script src="assets/vendor/daterangepicker/daterangepicker.js"></script>
+	<script src="<?php echo base_url();?>assets/vendor/daterangepicker/moment.min.js"></script>
+	<script src="<?php echo base_url();?>assets/vendor/daterangepicker/daterangepicker.js"></script>
 <!--===============================================================================================-->
-	<script src="assets/vendor/countdowntime/countdowntime.js"></script>
+	<script src="<?php echo base_url();?>assets/vendor/countdowntime/countdowntime.js"></script>
 <!--===============================================================================================-->
-	<script src="assets/js/main_login.js"></script>
+	<script src="<?php echo base_url();?>assets/js/main_login.js"></script>
 
 </body>
 </html>
