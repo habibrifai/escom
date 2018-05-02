2d573ccbdc8d60e1aa76e7972d1f4128f47c5b03
Hipzul Achmad Jabbar <hipzulachmadjabbar@gmail.com>
2018-05-02 23:11:24 +0700

    Maap ngasal wkwk

---
+++ application/controllers/perusahaan/dashboard.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller {
+
+	function __construct(){
+		parent::__construct();
+		$this->load->model('m_perusahaan');
+		$this->load->model('m_organisasi');
+	}
+
+	public function index(){
+    
+		$this->load->view('perusahaan/dashboard');
+	}
+
+	public function list_spj(){
+		$this->load->view('admin/list_spj');
+	}
+
+}
+?>
