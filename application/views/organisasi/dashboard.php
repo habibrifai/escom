5cd842cb6d6cd6901170923c16012a37559dc44c
ITS\5215100059 <rifaihabib29@gmail.com>
2018-05-04 17:17:58 +0700

    test

--- application/views/organisasi/dashboard.php
+++ application/views/organisasi/dashboard.php
@@ -145,7 +145,7 @@ if ($this->session->userdata('status') != 'login organisasi') {
 				<div class="col-lg-12">
 					<h1 class="page-header">Dashboard</h1>
 				</div>
-			</div><!--/.row-->			
+			</div>
 
 			<div class="panel panel-container">
 				<div class="row">
